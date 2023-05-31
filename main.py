f930931f4f984d2af711798133075cf15f7ef698
Morg <morg@morgs9.xyz>
2023-05-31 18:11:50 -0400

    Added Weather Output (i promise i'll work on database soon)

--- main.py
+++ main.py
@@ -19,27 +19,35 @@ weather = requests.get(
 airquality = requests.get(
     "https://api.openweathermap.org/data/2.5/air_pollution", params=parameters, timeout=20)
 
-levels = airquality.json()["list"][0]["components"]
+location = weather.json()["name"]
+weatherData = weather.json()["main"]
 
 aqi = airquality.json()["list"][0]["main"]["aqi"]
+airData = airquality.json()["list"][0]["components"]
 
 print(
     "Timestamp: " + str(datetime.now().isoformat()),
     "\n",
+    "Location: " + location,
+    "\n\n",
     "Air Quality Index: " + str(aqi),
     "\n",
+    "PM2.5: " + str(airData["pm2_5"]) + "μg/m3",
     "\n",
-    "PM2.5: " + str(levels["pm2_5"]) + "μg/m3",
+    "PM10: " + str(airData["pm10"]) + "μg/m3",
     "\n",
-    "PM10: " + str(levels["pm10"]) + "μg/m3",
+    "Carbon Monoxide: " + str(airData["co"]) + "μg/m3",
     "\n",
-    "Carbon Monoxide: " + str(levels["co"]) + "μg/m3",
+    "Nitrogen Dioxide: " + str(airData["no2"]) + "μg/m3",
     "\n",
-    "Nitrogen Dioxide: " + str(levels["no2"]) + "μg/m3",
+    "Ozone: " + str(airData["o3"]) + "μg/m3",
     "\n",
-    "Ozone: " + str(levels["o3"]) + "μg/m3",
-    "\n",
-    "Sulphur Dioxide: " + str(levels["so2"]) + "μg/m3",
+    "Sulphur Dioxide: " + str(airData["so2"]) + "μg/m3",
     "\n\n",
-    "(μg/m3 = Micrograms per meter cubed)"
+    "(μg/m3 = Micrograms per meter cubed)\n",
+)
+
+print(
+    "Weather:\nTemprature: " + str(round(weatherData["temp"])) + "°F",
+    "\nHumidity: " + str(round(weatherData["humidity"])) + "°F"
 )
