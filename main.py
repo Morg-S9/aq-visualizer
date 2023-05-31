69acdca5b5c0e2a06bacc6a955a53ae981206edd
Morg <56270390+Morg-S9@users.noreply.github.com>
2023-05-31 14:22:26 +0000

    Simplified output process Added timestamp output in ISO format

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import json
 import requests
 
@@ -5,7 +6,6 @@ def jprint(obj):
     # create a formatted string of the Python JSON object
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
-
 
 parameters = {
     "apikey": "bc1321f88951a4ba65bc02e181d4ca60",
@@ -17,34 +17,29 @@ parameters = {
 weather = requests.get(
     "https://api.openweathermap.org/data/2.5/weather", params=parameters, timeout=20)
 airquality = requests.get(
-    "http://api.openweathermap.org/data/2.5/air_pollution", params=parameters, timeout=20)
+    "https://api.openweathermap.org/data/2.5/air_pollution", params=parameters, timeout=20)
 
-aqlevels = airquality.json()["list"][0]
+levels = airquality.json()["list"][0]["components"]
 
-aqi = aqlevels["main"]["aqi"]
-
-pm2_5 = aqlevels["components"]["pm2_5"]
-pm10 = aqlevels["components"]["pm10"]
-co = aqlevels["components"]["co"]
-no2 = aqlevels["components"]["no2"]
-o3 = aqlevels["components"]["o3"]
-so2 = aqlevels["components"]["so2"]
+aqi = airquality.json()["list"][0]["main"]["aqi"]
 
 print(
+    "Timestamp: " + str(datetime.now().isoformat()),
+    "\n",
     "Air Quality Index: " + str(aqi),
     "\n",
     "\n",
-    "PM2.5: " + str(pm2_5) + "μg/m3",
+    "PM2.5: " + str(levels["pm2_5"]) + "μg/m3",
     "\n",
-    "PM10: " + str(pm10) + "μg/m3",
+    "PM10: " + str(levels["pm10"]) + "μg/m3",
     "\n",
-    "Carbon Monoxide: " + str(co) + "μg/m3",
+    "Carbon Monoxide: " + str(levels["co"]) + "μg/m3",
     "\n",
-    "Nitrogen Dioxide: " + str(no2) + "μg/m3",
+    "Nitrogen Dioxide: " + str(levels["no2"]) + "μg/m3",
     "\n",
-    "Ozone: " + str(o3) + "μg/m3",
+    "Ozone: " + str(levels["o3"]) + "μg/m3",
     "\n",
-    "Sulphur Dioxide: " + str(so2) + "μg/m3",
+    "Sulphur Dioxide: " + str(levels["so2"]) + "μg/m3",
     "\n\n",
     "(μg/m3 = Micrograms per meter cubed)"
 )
