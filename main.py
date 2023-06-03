26b5504620a8fafb5448fea81bc6c61ff8e85578
Morg <morg@morgs9.xyz>
2023-06-03 13:08:19 -0400

    Finished database commands Set rain and snow ID flags Looped program Added basic console outputs Updated database table commands

--- main.py
+++ main.py
@@ -2,16 +2,7 @@ from datetime import datetime
 import json
 import requests
 import psycopg2
-
-precip_id = list(map(int, open("precip_id.txt", encoding="utf-8").read().splitlines()))
-
-conn = psycopg2.connect(database="data",
-                        host="192.168.1.9",
-                        user="root",
-                        password="1234",
-                        port="5432")
-db = conn.cursor()
-
+import time
 
 def jprint(obj):
     # create a formatted string of the Python JSON object
@@ -19,6 +10,27 @@ def jprint(obj):
     print(text)
 
 
+# Create lists for Rain and Snow weather IDs
+with open("precip_id.txt", 'r', encoding="utf-8") as data:
+    content = data.read()
+data_sets = content.split("\n\n")
+rainids = []
+snowids = []
+
+for item in data_sets[0].split("\n"):
+    rainids.append(int(item))
+for item in data_sets[1].split("\n"):
+    snowids.append(int(item))
+
+# Setup database connection
+conn = psycopg2.connect(database="data",
+                        host="192.168.1.9",
+                        user="root",
+                        password="1234",
+                        port="5432")
+db = conn.cursor()
+
+# Set API parameters
 parameters = {
     "apikey": "bc1321f88951a4ba65bc02e181d4ca60",
     "lat": "39.144379",
@@ -26,42 +38,83 @@ parameters = {
     "units": "imperial"
 }
 
+while True:
+    # Make API Requests
+    print("Making API Requests...")
+    weather = requests.get(
+        "https://api.openweathermap.org/data/2.5/weather", params=parameters, timeout=20)
+    airquality = requests.get(
+        "https://api.openweathermap.org/data/2.5/air_pollution", params=parameters, timeout=20)
+    
+    # Check status code for errors
+    if weather.status_code != 200 or airquality.status_code != 200:
+        print("\nAPI error. Status:\n"
+              + "Weather: " + str(weather.status_code) + "\n" + weather.text
+              + "\nAir Qual.: " + str(airquality.status_code) + "\n" + weather.text
+        )
+        exit()
+    else:
+        print("Done.\n")
 
-weather = requests.get(
-    "https://api.openweathermap.org/data/2.5/weather", params=parameters, timeout=20)
-airquality = requests.get(
-    "https://api.openweathermap.org/data/2.5/air_pollution", params=parameters, timeout=20)
+    # Sort through data
+    timestamp = "'" + datetime.now().isoformat() + "'"
+    location = weather.json()["name"]
+    weatherID = weather.json()["weather"][0]["id"]
+    envData = weather.json()["main"]
+    windData = weather.json()["wind"]
+    aqi = airquality.json()["list"][0]["main"]["aqi"]
+    airData = airquality.json()["list"][0]["components"]
 
-timestamp = "\"" + datetime.now().isoformat() + "\""
+    # Set precip flag
+    if weatherID in rainids or weatherID in snowids:
+        PRECIP = True
+    else:
+        PRECIP = False
 
-weatherID = weather.json()["weather"][0]["id"]
+    # Compile weather database command
+    db.execute("INSERT INTO weather VALUES ("
+            + timestamp
+            + "," + str(weatherID)
+            + "," + str(envData["temp"])
+            + "," + str(envData["temp_max"])
+            + "," + str(envData["temp_min"])
+            + "," + str(envData["humidity"])
+            + "," + str(windData["speed"])
+            + "," + str(windData["deg"])
+            + "," + str(envData["pressure"])
+            + "," + str(PRECIP) + ");"
+            )
 
-location = weather.json()["name"]
+    # Check if precip database command needs to be made
+    if PRECIP is True:
+        if weatherID in rainids:
+            precipData = weather.json()["rain"]
+        else:
+            precipData = weather.json()["snow"]
 
-envData = weather.json()["main"]
-windData = weather.json()["wind"]
+        if "3h" in precipData.keys():
+            h3precip = precipData["3h"]
+        else:
+            h3precip = "NULL"
+        db.execute("INSERT INTO precipitation VALUES ("
+                + timestamp
+                + "," + str(weatherID)
+                + "," + str(precipData["1h"])
+                + "," + str(h3precip) + ");"
+                )
 
-aqi = airquality.json()["list"][0]["main"]["aqi"]
-airData = airquality.json()["list"][0]["components"]
+    # Compile air quality database command
+    db.execute("INSERT INTO air VALUES ("
+            + timestamp
+            + "," + str(aqi)
+            + "," + str(airData["pm2_5"])
+            + "," + str(airData["pm10"])
+            + "," + str(airData["co"])
+            + "," + str(airData["no2"])
+            + "," + str(airData["o3"])
+            + "," + str(airData["so2"]) + ");")
 
-if weatherID in precip_id:
-    PRECIP = True
-else:
-    PRECIP = False
-
-# Figure out how to auto-null when wind gusts aren't present.
-
-
-print("INSERT INTO weather VALUES ("
-      + str(timestamp)
-      + "," + str(weatherID)
-      + "," + str(envData["temp"])
-      + "," + str(envData["temp_max"])
-      + "," + str(envData["temp_min"])
-      + "," + str(envData["humidity"])
-      + "," + str(windData["speed"])
-      + "," + str(windData["deg"])
-      + "," + "null"
-      + "," + str(envData["pressure"])
-      + "," + str(PRECIP) + ");"
-      )
+    # Commit changes to database and wait
+    conn.commit()
+    print("Data stored. Timestamp: " + timestamp)
+    time.sleep(3600)
